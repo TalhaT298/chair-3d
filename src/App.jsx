9d9d2d91e8a6f3bcaa1a8de79521c644cbb0ac5a
Talha <mdtalha2008@gmail.com>
2023-08-31 23:47:07 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import { Canvas } from '@react-three/fiber'
 import './App.css'
+import Experience from './components/Experience'
 
 function App() {
 
   return (
     <div className='App'>
-      Hello
+      <Canvas>
+        <Experience/>
+      </Canvas>
     </div>
-  )
+  );
 }
 
 export default App
---
+++ src/components/Experience.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { BoxBufferGeometry, MeshNormalMaterial } from 'three';
+
+const Experience = () => {
+    return (
+        <mesh>
+            <boxBufferGeometry />
+            <meshNormalMaterial/>
+        </mesh>
+    );
+};
+
+export default Experience;
