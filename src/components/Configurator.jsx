e9985d6060faad5c85dae7faa7ecf221c212ef03
Talha <mdtalha2008@gmail.com>
2023-09-08 19:52:08 +0600

    google font add/popins

--- src/components/Configurator.jsx
+++ src/components/Configurator.jsx
@@ -4,7 +4,23 @@ import React from 'react';
 const Configurator = () => {
     return (
         <div className='configurator'>
-            Hello
+            <div className='configurator__section'>
+                <div className='configurator__section__title'>
+                Chair material
+                </div>
+                <div className='configurator__section__values'>
+                    <div className='item'>
+                    <div className='item_label'>
+                        Leather
+                    </div>
+                    </div>
+                    <div className='item'>
+                    <div className='item_label'>
+                        Fabric
+                    </div>
+                    </div>
+                </div>
+            </div>
             
         </div>
     );
